0d78e2bbce77be6b9aa2599d0de4ed3658864974
Krisofferson Marini <ksmarini@gmail.com>
2025-01-29 17:27:07 -0400

    Ajustes no arquivo `variaveis.py`

--- variaveis.py
+++ variaveis.py
@@ -1,11 +1,24 @@
 """Variáveis primitivas"""
 
-nome = "Marini"
-idade = 44
-altura = 1.75
-estudante = True
+nome = "Marini" # str
+idade = 44 # int
+altura = 1.75 # float
+estudante = True # bool
+numero = "45" # str
 
 print(nome)
 print(idade)
 print(altura)
 print(estudante)
+print(nome + numero) # Concatenação
+
+
+# Linguagem de tipagem dinamica - não precisa informar o tipo de dado
+# Linguagem fortemante tipada - ela não aceita alguma operações entre tipos
+# diferentes
+
+# Embora não seja preciso, podemos também setar o tipo de dado
+meu_nome: str = "Marini"
+minha_idade: int = 18
+minha_altura: float = 1.90
+meu_numero: str = "73"
